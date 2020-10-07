7db17f496175baaf1273c8eba32299d97a3417f6
Bruno Saad <saadbruno@gmail.com>
2020-10-07 21:28:22 +0000

    adds filter dropdown and ability to hide places

---
+++ code/views/home/partials/layersDropdown.php
@@ -0,0 +1,13 @@
+<div class="dropdown-menu dropdown-menu-right" aria-labelledby="layersButton">
+
+    <div class="layersGroup">
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="placesCheckbox" checked>
+            <label class="form-check-label" for="placesCheckbox">
+                <i class="fas fa-map-marker-alt"></i> Marcações
+            </label>
+        </div>
+
+    </div>
+</div>
